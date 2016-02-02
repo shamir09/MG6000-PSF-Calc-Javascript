0cb8bfc81e429ccb79398b352ee6f9494d51f714
Shamir Ajate <shamirajate@gmail.com>
2016-02-01 23:25:20 -0500

    Added Jquery Animation to all ID's

---
+++ assets/animation.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+$(document).ready(function () {
+    $('#mrglasslogo').hide().fadeIn(1000);
+    $('#headTitle').hide().fadeIn(500);
+    $('#FL_Product').hide().fadeIn(600);
+    $('#contForm').hide().fadeIn(800).slideDown(900);
+ }
+);
