ecd48d354efbb516ea915b164d85a50c5c72232c
Shamir Ajate <shamirajate@gmail.com>
2016-04-05 23:50:56 -0400

    Span color change on Input field Focus

--- assets/animation.js
+++ assets/animation.js
@@ -5,5 +5,13 @@ $(document).ready(function () {
     $('#FL_Product').hide().slideDown(600);
     $('#contForm').hide().fadeIn(800).slideDown(900);
 
+      $('input').focus(function() {
+          $( this ).next( "span" ).css( "color", "#2196F3" );
+      });
+
+      $('input').blur(function() {
+          $( this ).next( "span" ).css( "color", "darkgray" );
+      });
+
  }
 );
