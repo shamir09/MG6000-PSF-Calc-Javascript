cb3a498c46bc3b2bbf90e690d5c499d2770024ed
Shamir Ajate <shamirajate@gmail.com>
2016-04-15 07:04:13 -0400

    Correct Showing J1 or J1 Alum image

--- assets/MG6000psfCalculator.js
+++ assets/MG6000psfCalculator.js
@@ -300,14 +300,16 @@ if (reinforcementSelected == 2)  {   // If Reinforcement M3 is selected then Use
 }
 
     ResetFields();
-
-    if(psf_M1 != undefined) {
+    if(psf_M1 != undefined || psf_M2 != undefined) {
           if(height < 120) { document.getElementById("PSF_Result_Div").innerHTML = psf_M1;
-                             document.getElementById("J1").style.display = "inline";                   // OPTION 1 - JAMB   - NO REINF
-                           }
-          else if(height >= 120)   {document.getElementById("J1_Alum").style.display = "inline";}      // OPTION 1 - JAMB   - ALUM REINF   after 120" Height as per Page 9 of 12.
-}
-    else if(psf_M2 != undefined) {  document.getElementById("PSF_Result_Div").innerHTML = psf_M2;  }
+                    document.getElementById("J1").style.display = "inline";        }     // OPTION 1 - JAMB   - NO REINF  }
+          else if(height >= 120)   {
+            document.getElementById("J1_Alum").style.display = "inline";           } // OPTION 1 - JAMB   - ALUM REINF   after 120" Height as per Page 9 of 12.
+    }
+
+/////////////// PRINT THE RESULT TO THE WINDLOAD CALCULATION ID ELEMENT  //////////////////////
+    if(psf_M1 != undefined) {  document.getElementById("PSF_Result_Div").innerHTML = psf_M1     }
+    if(psf_M2 != undefined) {  document.getElementById("PSF_Result_Div").innerHTML = psf_M2;    }
 
 
 
